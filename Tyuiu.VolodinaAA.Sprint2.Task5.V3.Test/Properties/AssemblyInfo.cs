8c1b98a04ad3b4e903cac8ffe61daed1e6b16a74
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-10-10 12:50:41 +0300

    Task5

---
+++ Tyuiu.VolodinaAA.Sprint2.Task5.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VolodinaAA.Sprint2.Task5.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VolodinaAA.Sprint2.Task5.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40dc5b78-068b-4897-bd5c-43588689c3de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VolodinaAA.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolodinaAA.Sprint2.Task5.V3.Lib;
+
+namespace Tyuiu.VolodinaAA.Sprint2.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#2 |Выполнила: Володина А.А.|ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#2                                                                *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Володина Александра Александровна | ПКТб-23-1                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int value;
+            Console.WriteLine("Введите номер дня недели: ");
+            value = Convert.ToInt32(Console.ReadLine());
+            string day;
+            if ((value < 1)||(value > 7))
+            {
+                day = "Неверно введенное значение!";
+            }
+            else
+            {
+                day = $"День недели с введенным номером {value} - " + ds.FindDayName(value);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(day);
+            Console.ReadKey();
+        }
+    }
+}
